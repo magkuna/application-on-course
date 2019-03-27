40f5b61ece1cd12da481977db92b17539838ef8c
magkuna <magkuna@gmail.com>
2019-03-27 10:19:02 +0100

    2 stage

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 class App {
     constructor(selector){
-        this.container = dpcument.querySelector(selector ||document.body) // gdzie wyświetla
-        this.numberOfUsers = 10
-        this.genderOfUsers = ''
-        this.searchTerm = ''
-        this.users = null // zadeklarowana nazwa zmiennej ale nic narazie w niej nie ma
+        this.container = document.querySelector(selector ||document.body) // 1gdzie wyświetla
+        this.numberOfUsers = 10 //2
+        this.genderOfUsers = '' //3
+        this.searchTerm = ''  //4
+        this.users = null // 5zadeklarowana nazwa zmiennej ale nic narazie w niej nie ma
 
-        this.render()// wywoływanie rendera
+        this.render()   // 6wywoływanie rendera
     }
     render (){
-        this.container.innerHTML = '' //czyszczenie kontenera 
+        this.container.innerHTML = '' //5czyszczenie kontenera 
         this.renderInput('number')
-        this.renderInput('type') //wywoływanie inputa gdzie i ile razy, w tym przypadku x2, i nadanie typów
+        this.renderInput('type') //10wywoływanie inputa gdzie i ile razy, w tym przypadku x2, i nadanie typów
                                     
         
     }
     renderInput(type){
-        const input = document.createElement('input') // tworzenie inputa
-        input.setAttribute('type', type) // nadanie typu inputowi
-        this.container.appendChild(input)//w czym umieszony
+        const input = document.createElement('input') // 7tworzenie inputa
+        input.setAttribute('type', type) // 9nadanie typu inputowi
+        input.addEventListener(
+            'input',
+            () => this.numberOfUsers = event.target.value //12 dobieramy się do numberofusers
+
+        )         // 11co ma robić input
+        this.container.appendChild(input)//8w czym umieszczony
     
        
     
